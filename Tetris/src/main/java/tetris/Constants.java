367e55bc287224dc7a33a583e1df198fc1f7e2d6
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-04-18 13:10:13 +0300

    Add fields

--- Tetris/src/main/java/tetris/Constants.java
+++ Tetris/src/main/java/tetris/Constants.java
@@ -2,10 +2,16 @@ package tetris;
 
 public class Constants {
     
-    private static final double VERSION = 0.10;
+    public static final double VERSION = 0.5;
     
-    public static double getVersion() {
-        return VERSION;
-    }
+    // GAME
+    public static final int BOARD_DEFAULT_HEIGHT = 18;
+    public static final int BOARD_DEFAULT_WIDTH = 10;
     
+    //UI
+    public static final int RECTANGLE_DEFAULT_SIZE = 30;
+    public static final int WINDOW_DEFAULT_WIDTH =
+            (BOARD_DEFAULT_WIDTH + 4) * (RECTANGLE_DEFAULT_SIZE + 2);
+    public static final int WINDOWS_DEFAULT_HEIGHT =
+            BOARD_DEFAULT_HEIGHT * (RECTANGLE_DEFAULT_SIZE + 2);
 }
